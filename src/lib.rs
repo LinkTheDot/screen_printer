dd9d14445c2f2d9e43e8f32701d78a85a5603123
LinkTheDot <CirnoStrongest9909@hotmail.com>
2022-11-26 23:33:06 -0500

    Adds documentation to everything
    In order to make the use of this program easier, this commit adds
    documentation for every existing method, enum, and struct.

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,38 @@
 use std::cmp::Ordering;
 use std::fmt::Display;
 
+/// These are the possible ways the program can fail.
+///
+/// Each error will contain 'ErrorData' which holds the
+/// expected and outcome results in the event of the error.
 #[derive(Debug)]
 pub enum PrintingError {
-  RowTooLong(ErrorData),
-  RowTooShort(ErrorData),
-  TooManyCharacters(ErrorData),
-  TooLittleCharacters(ErrorData),
-  RowsDontMatchLength,
+  RowTooLong(LengthErrorData),
+  RowTooShort(LengthErrorData),
+
+  /// In the context of creating a grid from a full list of characters
+  TooManyCharacters(LengthErrorData),
+  /// In the context of creating a grid from a full list of characters
+  TooLittleCharacters(LengthErrorData),
+
+  RowsDontMatchLengths,
 }
 
+/// The namespace for all methods used to create and print a grid
+// add this when that's implemented, remove before committing
+// ///
+// /// Also used to store data for the dynamic printing method
 pub struct Printer;
 
+/// Error data for when incorrect sizes for the are passed into a method
 #[derive(Debug)]
-pub struct ErrorData {
+pub struct LengthErrorData {
   pub expected_length: usize,
   pub got_length: usize,
 }
 
-impl ErrorData {
+impl LengthErrorData {
+  /// Creates a new LengthErrorData for the expected length and actual length
   #[allow(clippy::new_without_default)]
   pub fn new(expected_length: usize, got_length: usize) -> Self {
     Self {
@@ -33,47 +47,78 @@ impl ErrorData {
 }
 
 impl Printer {
+  // change this for when dynamic printing is added remove this comment before committing
+  /// Creates a new Printer, this is not needed for most methods since Printer
+  /// is only there for the namespace
   #[allow(clippy::new_without_default)]
   pub fn new() -> Self {
     Self {}
   }
 
-  pub fn create_grid_from_single_character<T>(
-    character: &T,
-    width: usize,
-    height: usize,
-  ) -> Result<String, PrintingError>
+  /// Creates a grid of the given size with the given character.
+  ///
+  /// It's recommended that the passed in item is only 1 character long.
+  ///
+  /// # Example
+  /// ```
+  /// use terminal_printing::*;
+  ///
+  /// let character = "a";
+  /// let expected_grid = "aaa\naaa\naaa";
+  ///
+  /// let grid = Printer::create_grid_from_single_character(&character, 3, 3);
+  ///
+  /// assert_eq!(expected_grid, grid);
+  /// ```
+  pub fn create_grid_from_single_character<T>(character: &T, width: usize, height: usize) -> String
   where
     T: Display,
   {
     let row = Self::get_row_of_character(character, width);
 
-    Self::create_grid_from_single_row(&row, width, height)
+    Self::create_grid_from_single_row(&row, height)
   }
 
-  pub fn create_grid_from_single_row<T>(
-    row: &T,
-    width: usize,
-    height: usize,
-  ) -> Result<String, PrintingError>
+  /// Creates a grid of the given height with the given row
+  ///
+  /// # Example
+  /// ```
+  /// use terminal_printing::*;
+  ///
+  /// let row = "abcd";
+  /// let expected_grid = "abcd\nabcd\nabcd";
+  ///
+  /// let grid = Printer::create_grid_from_single_row(&row, 3);
+  ///
+  /// assert_eq!(expected_grid, grid);
+  /// ```
+  pub fn create_grid_from_single_row<T>(row: &T, height: usize) -> String
   where
     T: Display,
   {
     let row = format!("{}", row);
-    let row_size = row.chars().count();
 
-    match row_size.cmp(&width) {
-      Ordering::Less => Err(PrintingError::RowTooShort(ErrorData::new(row_size, width))),
-      Ordering::Greater => Err(PrintingError::RowTooLong(ErrorData::new(row_size, width))),
-      Ordering::Equal => Ok(
-        (0..height)
-          .fold(String::new(), |screen, _| format!("{}\n{}", screen, row))
-          .trim()
-          .to_string(),
-      ),
-    }
+    (0..height)
+      .fold(String::new(), |screen, _| format!("{}\n{}", screen, row))
+      .trim()
+      .to_string()
   }
 
+  /// Creates a grid of the given size with the given list of characters
+  ///
+  /// An error is returned when the grid size doesn't match the amount of characters given
+  ///
+  /// # Example
+  /// ```
+  /// use terminal_printing::*;
+  ///
+  /// let characters = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+  /// let expected_grid = "abc\ndef\nghi";
+  ///
+  /// let grid = Printer::create_grid_from_full_character_list(&characters, 3, 3).unwrap();
+  ///
+  /// assert_eq!(expected_grid, grid);
+  /// ```
   pub fn create_grid_from_full_character_list<T>(
     characters: &Vec<T>,
     width: usize,
@@ -85,11 +130,11 @@ impl Printer {
     let grid_size = width * height;
 
     match characters.len().cmp(&grid_size) {
-      Ordering::Less => Err(PrintingError::TooLittleCharacters(ErrorData::new(
+      Ordering::Less => Err(PrintingError::TooLittleCharacters(LengthErrorData::new(
         characters.len(),
         grid_size,
       ))),
-      Ordering::Greater => Err(PrintingError::TooManyCharacters(ErrorData::new(
+      Ordering::Greater => Err(PrintingError::TooManyCharacters(LengthErrorData::new(
         characters.len(),
         width * height,
       ))),
@@ -97,27 +142,63 @@ impl Printer {
     }
   }
 
-  pub fn create_grid_from_multiple_rows<T>(
-    rows: &[T],
-    width: usize,
-  ) -> Result<String, PrintingError>
+  /// Creates a grid of the given rows
+  ///
+  /// An error is returned if any of the rows isn't the same length as the first
+  ///
+  /// # Example
+  /// ```
+  /// use terminal_printing::*;
+  ///
+  /// let rows = vec![
+  ///   "abc",
+  ///   "def",
+  ///   "ghi",
+  /// ];
+  ///
+  /// let expected_grid = "abc\ndef\nghi";
+  ///
+  /// let grid = Printer::create_grid_from_multiple_rows(&rows).unwrap();
+  ///
+  /// assert_eq!(expected_grid, grid);
+  /// ```
+  pub fn create_grid_from_multiple_rows<T>(rows: &[T]) -> Result<String, PrintingError>
   where
     T: Display,
   {
     let rows: Vec<String> = rows.iter().map(|row| format!("{}", row)).collect();
+    let width = rows[0].chars().count();
 
     if Self::rows_have_same_lengths(&rows, width) {
       Ok(rows.join("\n"))
     } else {
-      Err(PrintingError::RowsDontMatchLength)
+      Err(PrintingError::RowsDontMatchLengths)
     }
   }
 
+  /// Moves the cursor up by the given height and prints the given grid.
+  ///
+  /// This is for printing over the previously printed grid.
+  /// It's recommended to add some whitespace before your first print so the grid
+  /// doesn't print into anything that was printed before this method was called.
+  ///
+  /// # Example
+  /// ```
+  /// use terminal_printing::*;
+  ///
+  /// let height = 10;
+  /// let width = 10;
+  /// let grid = Printer::create_grid_from_single_character(&"a", width, height);
+  ///
+  /// print!("{}", "\n".repeat(height + 5)); // add some space for the grid
+  /// Printer::print_over_previous_grid(grid, height);
+  /// ```
   pub fn print_over_previous_grid(grid: String, height: usize) {
     print!("\x1b[{};A", height);
     print!("\r{}", grid);
   }
 
+  /// Creates a row of the given width with the given character.
   fn get_row_of_character<T>(character: &T, width: usize) -> String
   where
     T: Display,
@@ -125,11 +206,13 @@ impl Printer {
     (0..width).fold(String::new(), |row, _| format!("{}{}", row, character))
   }
 
+  /// Returns true of all given rows have the same amount of characters as the expected input.
   fn rows_have_same_lengths(rows: &[String], expected_width: usize) -> bool {
     rows.iter().all(|row| row.chars().count() == expected_width)
   }
 }
 
+/// Creates a grid of the given width out of the given 2D grid of characters.
 fn create_grid_from_characters<T>(characters: &[T], width: usize) -> String
 where
   T: Display,
@@ -141,7 +224,7 @@ where
         .iter()
         .map(|character| format!("{}", character))
         .collect::<String>()
-        + "\n"
     })
-    .collect()
+    .collect::<Vec<String>>()
+    .join("\n")
 }
